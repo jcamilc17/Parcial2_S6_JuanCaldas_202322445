44e94cae8b781c45fcc7b05f3d778dd23d4cec85
Juan Caldas <jc.caldas23@uniandes.edu.co>
2025-05-19 16:56:02 -0500

    ⚡️ Implementacion Inicial de detalle aerolinea

--- src/aerolinea/detalleAerolinea/detalleAerolinea.component.ts
+++ src/aerolinea/detalleAerolinea/detalleAerolinea.component.ts
@@ -1,7 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-detalleAerolinea',
+  imports: [CommonModule],
   templateUrl: './detalleAerolinea.component.html',
   styleUrls: ['./detalleAerolinea.component.css']
 })
